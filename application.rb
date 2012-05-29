6e8630c3171a6f6806971563fec38fea826a3329
Bèr Kessels <ber@webschuur.com>
2012-05-29 23:33:24 +0200

    posting and re-rendering articles workposting and re-rendering articles workss

--- application.rb
+++ application.rb
@@ -2,6 +2,12 @@ require "rubygems"
 require "sinatra"
 require "data_mapper"
 require "dm-sqlite-adapter"
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
 # need install dm-sqlite-adapter
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/questions.db")
 
@@ -20,8 +26,29 @@ DataMapper.finalize
 # automatically create the question table
 Question.auto_upgrade!
 
+before do
+  content_type :html, 'charset' => 'utf-8'
+end
+
+# get the latest 20 Questions
 get '/' do
-    # get the latest 20 posts
-    @questions = Question.all(:order => [ :id.desc ], :limit => 20)
-    erb :index
+  @questions = Question.all(:order => [ :id.desc ], :limit => 20)
+  erb :index
+end
+
+#Make a new question
+post '/question' do
+  flash = {}
+
+  # Quick roundtrip for basic requirement checking on the form.
+  %w{name question}.each do |field|
+    flash[:error] = "#{field.capitalize} is required" if params[field].nil? or params[field] == ""
+  end
+
+  # Requirements met? then try to save the post.
+  if flash[:error].nil?
+    Question.new(:name => params[:name], :question => params[:question], :created_at => Time.now).save!
+    flash[:message] = "Question saved. Thanks <em>#{ h params[:name] }</em>"
+  end
+  flash.inspect
 end
